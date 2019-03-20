5881e6ca24420bf88fe27dc22787e1acdeff9236
georgi.stefanov <georgi.stefanov2001@gmail.com>
2019-03-20 13:09:35 +0200

    Made the carousel code reusable and reorganised the js files

---
+++ js/carousel.js
@@ -0,0 +1,5 @@
+function change_carousel_img(first_img_src, second_img_src, third_img_src) {
+    $(".carousel_first_img").attr("src", first_img_src);
+    $(".carousel_second_img").attr("src", second_img_src);
+    $(".carousel_third_img").attr("src", third_img_src);
+}
---
+++ js/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    var navbar = "#navbar_code";
+    $(navbar).load("../navbar.html"); //load the navbar code
+}); 
