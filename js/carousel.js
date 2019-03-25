0502b8d7672347dc15fcd627ae69ef3a88a27724
georgi.stefanov <georgi.stefanov2001@gmail.com>
2019-03-25 10:49:30 +0200

    Basically didn't fix anything

--- js/carousel.js
+++ js/carousel.js
@@ -1,5 +1,25 @@
-function change_carousel_img(first_img_src, second_img_src, third_img_src) {
-    $(".carousel_first_img").attr("src", first_img_src);
-    $(".carousel_second_img").attr("src", second_img_src);
-    $(".carousel_third_img").attr("src", third_img_src);
+function change_carousel_img(first_img_src, second_img_src, third_img_src, movie) {
+    var car_first_img_id = movie+"_carousel_first_img";
+    var car_second_img_id = movie+"_carousel_second_img";
+    var car_third_img_id = movie+"_carousel_third_img";
+
+    var car_id = movie+"_carousel_id";
+
+    $("#custom_carousel").attr("id",car_id);
+    $("#custom_carousel_prev").attr("href","#"+car_id);
+    $("#custom_carousel_next").attr("href","#"+car_id);
+
+    alert($("#"+car_id).attr("id"));
+    alert($("#custom_carousel_prev").attr("href"));
+    alert($("#custom_carousel_next").attr("href"));
+
+    $("#carousel_first_img").attr("id",car_first_img_id);
+    $("#carousel_second_img").attr("id",car_second_img_id);
+    $("#carousel_third_img").attr("id",car_third_img_id);
+    
+    $("#"+car_first_img_id).attr("src", first_img_src);
+    $("#"+car_second_img_id).attr("src", second_img_src);
+    $("#"+car_third_img_id).attr("src", third_img_src);
 }
+
+//toq js file e pulna mizeriq
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var navbar = "#navbar_code";
-    $(navbar).load("../navbar.html",function() {
+    $(navbar).load("navbar.html",function() {
         change_active_page();
     }); //load the navbar code
 }); 
