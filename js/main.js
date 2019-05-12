a4e7b1f5542129ac9838fe1ba2323e35e86626fc
georgi.stefanov <georgi.stefanov2001@gmail.com>
2019-05-12 22:24:15 +0300

    Remade the design (still need to refactor the code and redesign each movie page)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
     var navbar = "#navbar_code";
-    $(navbar).load("navbar.html",function() {
-        change_active_page();
-    }); //load the navbar code
+    $(navbar).load("navbar.html");
 }); 
